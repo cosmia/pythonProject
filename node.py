304d05e513b7cd361f36d9bb00cae06b8cbaa598
cosmia <angela.czubak@gmail.com>
2013-12-24 13:54:23 +0100

    Zdefiniowano klasę Node. Będzie ona opisywała węzeł w drzewie TRIE, które to z kolei zostanie przekształcone w automat wyszukujący zadane wzorce. Na razie brak dokumentacji do klasy i odpowiadającego jej wyjątku, brak też testów.

---
+++ node.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class NodeException(Exception):
+    def __init__(self, value):
+	self.napis = value
+    def __str__(self):
+	return repr(self.napis)
+    __repr__ = __str__
+
+class Node:
+    def __init__(self):
+	self.accept = None
+	self.edges = {}
+	self.fail = None
+    def labelCorrect(self, label):
+	if not isinstance(label, str):
+	    raise NodeException("label must be a character")
+	if len(label) != 1:
+	    raise NodeException("label must be exactly one character long")
+    def nodeCorrect(self, node, n=1):
+	if not isinstance(node, Node):
+	    lan = "the "
+	    if n != 1: lan += "second "
+	    lan += "argument must be a node"
+	    raise NodeException(lan)
+    def getAccept(self):
+	return self.accept
+    def getLabels(self):
+	return edges.keys
+    def getAim(self, label):
+	self.labelCorrect(label)
+	if label not in self.edges:
+	    if self.fail is None:
+		return self
+	    else:
+		return None
+	else:
+	    return self.edges[label]
+    def getFail(self):
+	return self.fail
+    def setAccept(self, number):
+	if not isinstance(number, (long, int)):
+	    raise NodeException("argument should be an integer or long")
+	self.accept = number
+    def setAim(self, label, node):
+	self.labelCorrect(label)
+	self.nodeCorrect(node,2)
+	self.edges[label] = node
+    def setFail(self, node):
+	self.nodeCorrect(node)
+	self.fail = node
