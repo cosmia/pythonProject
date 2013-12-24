da6a51c2d6ae4b6f161d3d4d10839de950ff4980
cosmia <angela.czubak@gmail.com>
2013-12-24 15:09:04 +0100

    komentarz opisujący samę klasę Node

--- node.py
+++ node.py
@@ -12,6 +12,7 @@ class NodeException(Exception):
     __repr__ = __str__
 
 class Node:
+    '''klasa opisująca wezel/stan w automacie/drzewie Trie'''
     def __init__(self):
 	'''konstruktor bezargumentowy
 	   accept, fail = None
