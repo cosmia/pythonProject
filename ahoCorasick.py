d5697dada721b119d35749340ebc6fe1a09e6026
cosmia <angela.czubak@gmail.com>
2013-12-25 19:05:08 +0100

    AhoCorasick: dodawanie słowa do drzewa

--- ahoCorasick.py
+++ ahoCorasick.py
@@ -18,12 +18,35 @@ class AhoCorasick:
 	'''konstruktor bezargumentowy
 	   n - korzen drzewa, pusty
 	   words - pusta liczba slow zakodowanych w drzewie'''
-	   self.n = Node()
-	   self.words = []
+	self.n = Node()
+	self.words = []
     def addWord(self, word):
 	'''dodaje slowo word do automatu/drzewa
 	   rzuca AhoCorasickException, jesli word nie jest stringiem'''
-	pass
+	if not isinstance(word, str):
+	    raise AhoCorasickException("argument nie jest stringiem")
+	dl = len(word)
+	if dl == 0: return #nie dodajemy pustego slowa
+	wezel = self.n
+	i = 0
+	#idziemy dopoki mozemy po istniejacych wezlach
+	while i < dl:
+	    labels = wezel.getLabels()
+	    if s[i] in labels:
+		wezel = wezel.getAim(s[i])
+	    else:
+		break
+	    i += 1
+	#a teraz tworzymy nowe, jesli taka potrzeba
+	while i < dl:
+	    wezel.setAim(s[i], Node())
+	    wezel = wezel.getAim(s[i])
+	    i += 1
+	#jesli jeszcze nie dodalismy tego slowa
+	if wezel.getAccept() == set():
+	    ktore = len(self.words)
+	    wezel.setAccept(ktore)
+	    self.words.append(word)
     def lookUp(self, word):
 	'''sprawdza, czy dane slowo wystepuje w drzewie
 	   zwraca True, jesli tak; False wpp
