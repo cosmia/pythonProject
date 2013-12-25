2fba99a2caf6df172b2911329ade4ae290de001c
cosmia <angela.czubak@gmail.com>
2013-12-25 20:12:41 +0100

    Wstępny zarys testów dla klasy AhoCorasick

---
+++ ahoTest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from ahoCorasick import *
+import unittest
+
+class TestAho(unittest.TestCase):
+    '''kod testujacy klase AhoCorasick'''
+    def setUp(self):
+	pass
+    def test_init(self):
+	'''sprawdzam dzialanie konstruktora'''
+	pass
+    def test_tree(self):
+	'''sprawdzam dodawanie i wyszukiwanie slow w drzewie'''
+	pass
+    def tearDown(self):
+	pass
+
+if __name__ == "__main__":
+    unittest.main()
